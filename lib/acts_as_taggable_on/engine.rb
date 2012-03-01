ae17be722d1430f96737cd1f407b92a4c8ca8d07
Joseph DelCioppio <Joseph.DelCioppio@gmail.com>
2012-03-01 11:53:55 -0500

    Update the helper file to exist with the engine application as opposed to being a library file.

--- lib/acts_as_taggable_on/engine.rb
+++ lib/acts_as_taggable_on/engine.rb
@@ -5,11 +5,5 @@ module ActsAsTaggableOn
       ActiveRecord::Base.extend ActsAsTaggableOn::Taggable
       ActiveRecord::Base.send :include, ActsAsTaggableOn::Tagger
     end
-
-    #initializer 'ActsAsTaggableOn AcrtionView' do |app|
-      #ActiveSupport.on_load(:action_view) do
-        #include ActsAsTaggableOn::TagsHelper
-      #end
-    #end
   end
 end
